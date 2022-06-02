7d06321669ddda97de7d7b3225e50a91f08d0713
Goutham Krishna K V <gauthamkrishna9991@live.com>
2022-06-02 15:06:12 +0530

    feat!: Part 1 - Benford's Analysis

--- src/graphical_grazer/graph.py
+++ src/graphical_grazer/graph.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Optional
 
 from graphical_grazer.node import Node
 
@@ -10,21 +10,39 @@ class Graph:
         self.vertices = {}
 
     def clear_nodes(self):
+        # For all vertices
         for i in self.vertices.values():
+            # Clear the "next element" variable and the visited flag.
             i.visited = False
             i.next_ele = None
 
     def visit(self, ele: int):
+        """
+        `visit` Method
+
+        This contains the special case where the elements were already created but 
+        cleared of visiting. This is useful in cases where one needs to iterate through
+        the graph again without regenerating the entire graph.
+        """
+        # "element is not created" situation
         if ele not in self.vertices:
+            # Create the element
             self.vertices[ele] = Node(ele)
-        if self.vertices[ele].visited:
-            return ele
-        self.vertices[ele].visited = True
-        nele = None
-        if ele % 2 == 0:
-            nele = int(ele / 2)
-        else:
-            nele = int(3 * ele + 1)
-        self.visit(nele)
-        self.vertices[ele].next_ele = self.vertices[nele]
-        return ele
+        # If element is not visited
+        if not self.vertices[ele].visited:
+            # Update the current element to visited
+            self.vertices[ele].visited = True
+            # Initialize next element to none
+            nele: Optional[int] = None
+            # If current element is even
+            if ele % 2 == 0:
+                # Take half
+                nele = int(ele / 2)
+            # If the current element is odd
+            else:
+                # Else thrice the element and add 1
+                nele = int(3 * ele + 1)
+            # Visit the next element
+            self.visit(nele)
+            # Join the "next" for current to the next element
+            self.vertices[ele].next_ele = self.vertices[nele]
